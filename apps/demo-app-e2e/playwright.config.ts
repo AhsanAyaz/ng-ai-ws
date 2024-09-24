afebeaeea1c3e07b24d22bd944e8f36e0f12fa23
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-09-24 20:58:09 +0200

    fix(ci): hardcode baseURL in playwright config

--- apps/demo-app-e2e/playwright.config.ts
+++ apps/demo-app-e2e/playwright.config.ts
@@ -4,7 +4,12 @@ import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['NX_BASE_URL'] || 'http://localhost:4200';
+let baseURL = process.env.BASE_URL;
+if (!baseURL && process.env.CI) {
+  baseURL = 'https://ahsanyaz.github.io/ng-gc-ws';
+} else {
+  baseURL = 'http://localhost:4200';
+}
 console.log({ env: process.env });
 console.log({ baseURL });
 /**
